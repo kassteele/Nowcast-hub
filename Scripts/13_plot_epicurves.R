83584e00735394f6c4fd7285455fd8d711b7e232
Jan van de Kassteele <jan.van.de.kassteele@rivm.nl>
2021-11-14 06:36:01 +0100

    Check added to prevent overwriting existing file

--- Scripts/13_plot_epicurves.R
+++ Scripts/13_plot_epicurves.R
@@ -5,39 +5,56 @@
 # Show progress
 message("Plot epicurves")
 
-plot_epicurves <- map(
-  .x = epicurve_data %>%
-    split(
-      f = list(.$location, .$age_group),
-      drop = TRUE,
-      sep = "_"),
-  .f = ~ ggplot(
-    data = .x,
-    mapping = aes(x = date)) +
-    geom_ribbon(
-      mapping = aes(ymin = N_quantile_0.025, ymax = N_quantile_0.975),
-      fill = 4,
-      alpha = 0.5) +
-    geom_line(
-      mapping = aes(y = N_rep),
-      col = "grey") +
-    geom_line(
-      mapping = aes(y = N_true)) +
-    geom_line(
-      mapping = aes(y = N_quantile_0.5),
-      colour = 4) +
-    scale_x_date(
-      date_breaks = "1 week",
-      minor_breaks = NULL,
-      date_labels = "%e %b",
-      expand = expansion(add = c(1, 1))) +
-    scale_y_continuous(
-      limits = c(0, NA),
-      expand = expansion(add = c(1, 1))) +
-    labs(
-      title = str_glue("{.x$location} {.x$age_group}")) +
-    theme_minimal() +
-    theme(
-      axis.text.x = element_text(
-        angle = 90,
-        hjust = 1, vjust = 0.5)))
+# This is what the plots have in common
+plot_epicurves_common <- list(
+  geom_ribbon(
+    mapping = aes(x = date, ymin = N_quantile_0.025, ymax = N_quantile_0.975),
+    fill = 4,
+    alpha = 0.5),
+  geom_line(
+    mapping = aes(x = date, y = N_rep),
+    col = "grey"),
+  geom_line(
+    mapping = aes(x = date, y = N_true)),
+  geom_line(
+    mapping = aes(x = date, y = N_quantile_0.5),
+    colour = 4),
+  scale_x_date(
+    date_breaks = "2 weeks",
+    date_minor_breaks = "1 week",
+    date_labels = "%e %b",
+    expand = expansion(add = c(1, 1))),
+  scale_y_continuous(
+    limits = c(0, NA),
+    expand = expansion(add = c(1, 1))),
+  labs(
+    x = "Meldedatum",
+    y = "7-day hospitalization incidence"),
+  facet_wrap(
+    facets = vars(location, age_group),
+    ncol = 2,
+    scales = "free_y",
+    labeller = label_wrap_gen(multi_line = FALSE)),
+  theme_minimal(),
+  theme(
+    axis.text.x = element_text(
+      angle = 90,
+      hjust = 1, vjust = 0.5)))
+
+# Epicurve total
+plot_epicurves_total <- ggplot(
+  data = epicurve_data %>%
+    filter(location == "DE" & age_group == "00+")) +
+  plot_epicurves_common
+
+# Epicurves by age group
+plot_epicurves_agegroup <- ggplot(
+  data = epicurve_data %>%
+    filter(location == "DE" & age_group != "00+")) +
+  plot_epicurves_common
+
+# Epicurves by location
+plot_epicurves_location <- ggplot(
+  data = epicurve_data %>%
+    filter(location != "DE" & age_group == "00+")) +
+  plot_epicurves_common
--- Scripts/14_export_csv_file.R
+++ Scripts/14_export_csv_file.R
@@ -8,22 +8,28 @@ message("Export csv file")
 # Specifications:
 # https://github.com/KITmetricslab/hospitalization-nowcast-hub/wiki/Data-format
 
-nowcast_data %>%
-  rename(
-    target_end_date = date) %>%
-  filter(
-    target_end_date >= forecast_date - 28) %>%
-  mutate(
-    forecast_date = forecast_date,
-    target = str_glue("{target_end_date - forecast_date} day ahead inc hosp"),
-    pathogen = "COVID-19") %>%
-  pivot_longer(
-    cols = starts_with("N_"),
-    names_sep = "_",
-    names_to = c(NA, "type", "quantile"),
-    values_to = "value") %>%
-  select(
-    location, age_group, forecast_date, target_end_date, target, type, quantile, value, pathogen) %>%
-  write_csv(
-    file = str_glue("Export/{forecast_date}-RIVM-KEW.csv"),
-    quote = "none")
+# Filename for given forecast_date
+filename <- str_glue("Export/{forecast_date}-RIVM-KEW.csv")
+
+# If the csv for the given forecast_date does not exists, create one
+if (!file.exists(filename)) {
+  nowcast_data %>%
+    rename(
+      target_end_date = date) %>%
+    filter(
+      target_end_date >= forecast_date - 28) %>%
+    mutate(
+      forecast_date = forecast_date,
+      target = str_glue("{target_end_date - forecast_date} day ahead inc hosp"),
+      pathogen = "COVID-19") %>%
+    pivot_longer(
+      cols = starts_with("N_"),
+      names_sep = "_",
+      names_to = c(NA, "type", "quantile"),
+      values_to = "value") %>%
+    select(
+      location, age_group, forecast_date, target_end_date, target, type, quantile, value, pathogen) %>%
+    write_csv(
+      file = filename,
+      quote = "none")
+}
