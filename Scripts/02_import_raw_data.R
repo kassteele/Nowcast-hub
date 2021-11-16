e103948894176ffcbc7f99657cde71b81bfe6cd0
Jan van de Kassteele <jan.van.de.kassteele@rivm.nl>
2021-11-16 16:22:25 +0100

    added check for (no) data update on nowcast hub repos

--- Scripts/02_import_raw_data.R
+++ Scripts/02_import_raw_data.R
@@ -9,3 +9,9 @@ message("Import raw data")
 reporting_data_raw <- read_csv(
   file = "https://github.com/KITmetricslab/hospitalization-nowcast-hub/blob/main/data-truth/COVID-19/COVID-19_hospitalizations_preprocessed.csv?raw=true",
   show_col_types = FALSE)
+
+# Stop if there is no new data
+if (reporting_data_raw %>% pull(date) %>% last() != today()) {
+  rm(reporting_data_raw)
+  stop("Raw reporting triangle data has not been updated yet")
+}
