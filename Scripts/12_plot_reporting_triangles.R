2b9d68ad174e65aaae8e5ba39db6e5787764e484
Jan van de Kassteele <jan.van.de.kassteele@rivm.nl>
2021-11-16 16:20:42 +0100

    rewrite plot reporting triangles

--- Scripts/12_plot_reporting_triangles.R
+++ Scripts/12_plot_reporting_triangles.R
@@ -5,43 +5,51 @@
 # Show progress
 message("Plot reporting triangles")
 
-# Create plots
-plot_reporting_triangles <- map(
-  .x = reporting_data %>%
-    split(
-      f = list(.$location, .$age_group),
-      drop = TRUE,
-      sep = "_"),
-  .f = ~ ggplot(
-    data = .x,
-    mapping = aes(x = date, y = delay, fill = n_rep)) +
-    geom_raster() +
-    scale_x_date(
-      date_breaks = "1 week",
-      date_minor_breaks = "1 day",
-      date_labels = "%e %b",
-      expand = expansion(add = c(1, 1))) +
-    scale_y_continuous(
-      expand = expansion(add = c(1, 1))) +
-    scale_fill_continuous_sequential(
-      palette = "Blues",
-      trans = "log1p") +
-    coord_equal() +
-    labs(
-      title = str_glue("{.x$location} {.x$age_group}")) +
-    theme_minimal() +
-    theme(
-      axis.text.x = element_text(
-        angle = 90,
-        hjust = 1, vjust = 0.5)))
+# This is what the plots have in common
+plot_reporting_triangles_common <- list(
+  geom_raster(
+    mapping = aes(x = date, y = delay, fill = n_rep)),
+  scale_x_date(
+    date_breaks = "2 weeks",
+    date_minor_breaks = "1 week",
+    date_labels = "%e %b",
+    expand = expansion(add = c(1, 1))),
+  scale_y_continuous(
+    expand = expansion(add = c(1, 1))),
+  scale_fill_continuous_sequential(
+    palette = "Blues",
+    na.value = "grey75",
+    breaks = c(0, 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000),
+    trans = "log1p"),
+  coord_equal(),
+  labs(
+    x = "Meldedatum",
+    y = "Delay",
+    fill = "n"),
+  facet_wrap(
+    facets = vars(location, age_group),
+    ncol = 2,
+    labeller = label_wrap_gen(multi_line = FALSE)),
+  theme_minimal(),
+  theme(
+    axis.text.x = element_text(
+      angle = 90,
+      hjust = 1, vjust = 0.5)))
 
-# # Open connection to multipage pdf
-# pdf(
-#   file = "Figures/reporting_triangles.pdf",
-#   onefile = TRUE)
-#
-# # Print
-# plot_reporting_trianangles
-#
-# # Close connection to pdf
-# dev.off()
+# Reporting triangle total
+plot_reporting_triangles_total <- ggplot(
+  data = reporting_data %>%
+    filter(location == "DE" & age_group == "00+")) +
+  plot_reporting_triangles_common
+
+# Reporting triangles by age group
+plot_reporting_triangles_agegroup <- ggplot(
+  data = reporting_data %>%
+    filter(location == "DE" & age_group != "00+")) +
+  plot_reporting_triangles_common
+
+# Reporting triangles by location
+plot_reporting_triangles_location <- ggplot(
+  data = reporting_data %>%
+    filter(location != "DE" & age_group == "00+")) +
+  plot_reporting_triangles_common
