e6d2499a84e779b4c54f678d28b0a4e279b0dd81
kassteele <jan.van.de.kassteele@rivm.nl>
2021-12-09 09:29:38 +0100

    no need to specify k = 2 explicitly

--- Scripts/08_1_fit_gam_DE_00.R
+++ Scripts/08_1_fit_gam_DE_00.R
@@ -11,7 +11,7 @@ reporting_gam_DE_00 <- bam(
     s(delay_trans, bs = "ps", k = 20) +
     s(day, bs = "re") +
     s(day_report, bs = "re") +
-    s(I_delay_max, bs = "re", k = 2),
+    s(I_delay_max, bs = "re"),
   family = nb,
   data = reporting_fit_data_DE_00,
   discrete = TRUE,
