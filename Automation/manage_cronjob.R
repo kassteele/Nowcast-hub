180c11b767867e5c9f9764561cb8f5e9f7aa4bc1
Jan van de Kassteele <jan.van.de.kassteele@rivm.nl>
2021-12-18 06:38:46 +0100

    Start cronjob at 6:00

--- Automation/manage_cronjob.R
+++ Automation/manage_cronjob.R
@@ -14,7 +14,7 @@ cron_add(
     workdir = "/home/kasstvdj/Scripts/Nowcast-hub",
     log_append = FALSE),
   frequency = "daily",
-  at = "8:00",
+  at = "6:00",
   id = "nowcast_hub",
   ask = FALSE)
 
