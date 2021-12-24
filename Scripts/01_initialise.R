bd1803320fd68035c96760d3ff6631689652e032
Jan van de Kassteele <jan.van.de.kassteele@rivm.nl>
2021-12-24 12:57:32 +0100

    max delay 80 days -> 42 days

--- Scripts/01_initialise.R
+++ Scripts/01_initialise.R
@@ -23,7 +23,7 @@ Sys.setlocale(category = "LC_TIME", locale = "en_US.UTF-8")
 options(lubridate.week.start = 1)
 
 # Set maximum delay
-max_delay <- 80L
+max_delay <- 42L
 
 # Number of Monte Carlo simulations for nowcast
 n_sim <- 1000
